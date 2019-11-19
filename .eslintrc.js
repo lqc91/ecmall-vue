ccaff706f64cbf9391d0d96f070eb61878fa0b51
lufan <ericlufan@163.com>
2019-11-20 03:25:57 +0800

    modify config: install packages, config eslint, dir aliases, etc
    modify configurations
    
    modify eslint configurations
    
    modify configaration: config dir aliases, install fastclick

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,15 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 语句末尾使用分号（默认）
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2],
+    // 函数名或 function 关键字与左括号之间使用一致的空格
+    'space-before-function-paren': ['error', { 'anonymous': 'always', 'named': 'never', 'asyncArrow': 'always' }]
   },
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  devServer: {
+    // server 启动后自动打开浏览器
+    open: 'true'
+  },
+  chainWebpack: (config) => {
+    // 配置目录别名
+    config.resolve.alias.set('api', resolve('src/api')).set('assets', resolve('src/assets')).set('base', resolve('src/base')).set('components', resolve('components')).set('pages', resolve('src/pages'));
+  }
+};
