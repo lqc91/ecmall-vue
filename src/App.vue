f1bf9018e1691d7a184d42c5b3b812f621f1540c
lufan <ericlufan@163.com>
2019-11-22 23:21:06 +0800

    homepage framework construction

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="app">
-    app
-    <router-view />
+  <div id="app" class="g-container">
+    <div class="g-view-container">
+      <router-view />
+    </div>
+    <div class="g-footer-container"></div>
   </div>
 </template>
