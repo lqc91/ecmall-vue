223f05d9d48eb11338c75057d7fd9aeb64b1fe14
lufan <ericlufan@163.com>
2019-11-25 12:46:32 +0800

    Homepage: add tabbar component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,19 @@
     <div class="g-view-container">
       <router-view />
     </div>
-    <div class="g-footer-container"></div>
+    <div class="g-footer-container">
+      <c-tabbar />
+    </div>
   </div>
 </template>
+
+<script>
+import CTabbar from 'components/tabbar';
+
+export default {
+  name: 'App',
+  components: {
+    CTabbar
+  }
+};
+</script>
