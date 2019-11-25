4b2f231e7b549311a0ec6ae6f96998ce4e08155f
lufan <ericlufan@163.com>
2019-11-25 21:51:07 +0800

    pages & router configurations

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,49 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    // 一级入口
+    name: 'home',
+    path: '/home',
+    // 路由懒加载
+    component: () => import('pages/home'),
+    // 二级入口
+    children: [
+      {
+        name: 'home-product',
+        path: 'product/:id',
+        component: () => import('pages/product')
+      }
+    ]
+  },
+  {
+    name: 'category',
+    path: '/category',
+    component: () => import('pages/category')
+  },
+  {
+    name: 'cart',
+    path: '/cart',
+    component: () => import('pages/cart')
+  },
+  {
+    name: 'personal',
+    path: '/personal',
+    component: () => import('pages/personal')
+  },
+  {
+    name: 'search',
+    path: '/search',
+    component: () => import('pages/search')
+  },
+  {
+    // 匹配任意路径
+    path: '*',
+    // 重定向
+    redirect: '/home'
+  }
+];
 
 const router = new VueRouter({
   mode: 'history',
