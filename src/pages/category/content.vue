05f5b9aa656ccf8a2564c3a60567f1bf92ba4639
lufan <ericlufan@163.com>
2019-12-03 13:10:12 +0800

    Category: add header and sidebar components

---
+++ src/pages/category/content.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>content</div>
+</template>
+
+<script>
+export default {
+  name: 'CategoryContent'
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/category/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <me-navbar class="header">
+    <template v-slot:center>搜索框</template>
+    <template v-slot:right>
+      <i class="iconfont icon-msg"></i>
+    </template>
+  </me-navbar>
+</template>
+
+<script>
+import MeNavbar from 'base/navbar';
+export default {
+  name: 'CategoryHeader',
+  components: {
+    MeNavbar
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/scss/mixins";
+
+.header {
+  &.mine-navbar {
+    background-color: $header-bgc-translucent;
+  }
+
+  .iconfont {
+    color: $icon-color-default;
+    font-size: $icon-font-size;
+  }
+}
+</style>
