0a6863b67aaaa3d0335f006240d3a3b5dfd7680f
lufan <ericlufan@163.com>
2019-12-05 23:16:17 +0800

    Search: add in & out animation and header component

---
+++ src/pages/search/header.vue
@@ -0,0 +1,48 @@
+<template>
+  <me-navbar class="header">
+    <template v-slot:left>
+      <i class="iconfont icon-back" @click="goBack"></i>
+    </template>
+    <template v-slot:center>
+      <me-search-box placeholder="开学季有礼，好货5折起" @query="query" />
+    </template>
+  </me-navbar>
+</template>
+
+<script>
+import MeNavbar from 'base/navbar';
+import MeSearchBox from 'base/search-box';
+
+export default {
+  name: 'SearchHeader',
+  components: {
+    MeNavbar,
+    MeSearchBox
+  },
+  methods: {
+    query(query) {
+      this.$emit('query', query);
+    },
+    goBack() {
+      this.$router.back();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/scss/mixins";
+
+.header {
+  border-bottom: 1px solid $border-color;
+
+  .iconfont {
+    color: $icon-color;
+    font-size: $icon-font-size;
+  }
+
+  .mine-search-box-wrapper {
+    background-color: #f7f7f7;
+  }
+}
+</style>
--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -1,12 +1,45 @@
 <template>
-  <div>search</div>
+  <transition name="search">
+    <div class="search">
+      <header class="g-header-container">
+        <search-header />
+      </header>
+      <div class="g-content-container"></div>
+    </div>
+  </transition>
 </template>
 
 <script>
+import SearchHeader from './header';
+
 export default {
-  name: 'Search'
+  name: 'Search',
+  components: {
+    SearchHeader
+  }
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "~assets/scss/mixins";
+
+.search {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: $search-z-index;
+  background-color: $bgc-theme;
+}
+
+.search-enter-active,
+.search-leave-active {
+  transition: all 0.3s;
+}
+
+.search-enter,
+.search-leave-to {
+  transform: translate3d(100%, 0, 0);
+}
 </style>
