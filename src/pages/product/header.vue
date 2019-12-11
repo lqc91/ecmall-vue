df6208a9dd03002f82d61c2e109cd1289768e043
lufan <ericlufan@163.com>
2019-12-11 13:32:28 +0800

    Product: add enter&leave animation, header component

---
+++ src/pages/product/header.vue
@@ -0,0 +1,47 @@
+<template>
+  <me-navbar class="header">
+    <template v-slot:left>
+      <i class="iconfont icon-back" @click="goBack"></i>
+    </template>
+    <template v-slot:center>
+      <p class="header-center">商品详情</p>
+    </template>
+    <template v-slot:right>
+      <router-link to="/cart" tag="i" class="iconfont icon-cart"></router-link>
+    </template>
+  </me-navbar>
+</template>
+
+<script>
+import MeNavbar from 'base/navbar';
+
+export default {
+  name: 'ProductHeader',
+  components: {
+    MeNavbar
+  },
+  methods: {
+    goBack() {
+      this.$router.back();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/scss/mixins";
+
+.header {
+  background-color: $header-bgc-translucent;
+
+  &-center {
+    color: #fff;
+    text-align: center;
+    font-size: $font-size-l;
+  }
+}
+.iconfont {
+  color: $icon-color-default;
+  font-size: $icon-font-size;
+}
+</style>
--- src/pages/product/index.vue
+++ src/pages/product/index.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="product">product</div>
+  <transition name="product">
+    <div class="product">
+      <header class="g-header-container">
+        <product-header />
+      </header>
+    </div>
+  </transition>
 </template>
 
 <script>
+import ProductHeader from './header';
 export default {
-  name: 'Product'
+  name: 'Product',
+  components: {
+    ProductHeader
+  }
 };
 </script>
 
@@ -20,5 +30,23 @@ export default {
   width: 100%;
   height: 100%;
   background-color: $bgc-theme;
+}
+.product-leave-active {
+  transition: transform 0.5s;
+}
+.product-leave-to {
+  transform: translateX(100%);
+}
+.product {
+  animation: product-enter 0.5s;
+}
+
+@keyframes product-enter {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
 }
 </style>
