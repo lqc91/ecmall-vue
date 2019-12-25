c32b40e26d603ea8c8318f94de02eaff7e8b0348
lufan <ericlufan@163.com>
2019-12-25 20:38:51 +0800

    Cart: add header component

---
+++ src/pages/cart/header.vue
@@ -0,0 +1,47 @@
+<template>
+  <me-navbar class="header">
+    <template v-slot:left>
+      <i class="iconfont icon-back" @click="goBack"></i>
+    </template>
+    <template v-slot:center>
+      <p class="header-center">购物车</p>
+    </template>
+    <template v-slot:right>
+      <i class="iconfont icon-msg"></i>
+    </template>
+  </me-navbar>
+</template>
+
+<script>
+import MeNavbar from 'base/navbar';
+
+export default {
+  name: 'CartHeader',
+  components: {
+    MeNavbar
+  },
+  methods: {
+    goBack() {
+      this.$router.back();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/scss/mixins";
+
+.header {
+  color: #fff;
+  border-bottom: 1px solid $border-color;
+  background-color: $header-bgc-translucent;
+}
+.header-center {
+  text-align: center;
+  margin-left: 34px;
+  font-size: $font-size-l;
+}
+.iconfont {
+  font-size: $icon-font-size;
+}
+</style>
--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>cart</div>
+  <transition name="cart">
+    <div class="cart">
+      <header class="g-header-container">
+        <cart-header />
+      </header>
+    </div>
+  </transition>
 </template>
 
 <script>
+import CartHeader from './header';
+
 export default {
-  name: 'Cart'
+  name: 'Cart',
+  components: {
+    CartHeader
+  }
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "~assets/scss/mixins";
+
+.cart-enter-active,
+.cart-leave-active {
+  transition: transform 0.5s;
+}
+.cart-enter,
+.cart-leave-to {
+  transform: translateX(100%);
+}
+.cart-enter-to,
+.cart-leave {
+  transform: translateX(0);
+}
+.cart {
+  width: 100%;
+  height: 100%;
+}
 </style>
