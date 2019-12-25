c65adb66ee5f6f303cd62fb625cf44ba79f8547c
lufan <ericlufan@163.com>
2019-12-25 21:03:06 +0800

    config devServer.proxy

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,21 @@ function resolve(dir) {
 module.exports = {
   devServer: {
     // server 启动后自动打开浏览器
-    open: 'true'
+    open: 'true',
+    host: 'localhost',
+    port: 8080,
+    // 在开发环境下将 API 请求代理到 API 服务器
+    proxy: {
+      '/mock': {
+        // 将以 /mock 开头的路径请求代理到 target
+        target: 'http://localhost:8080',
+        // 是否代理websockets
+        ws: false,
+        // 是否将主机头的原点改为目标的 URL
+        // changes the origin of the host header to the target URL
+        changeOrigin: false
+      }
+    }
   },
   chainWebpack: (config) => {
     // 配置目录别名
